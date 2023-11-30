86b13675c93b0954d7fb1f8383f3b14586878078
Julio Caio <julio.caio@academico.ifpb.edu.br>
2023-11-30 18:48:21 -0300

    Alterações no banner e card

--- src/Pages/PreviewCourse/PlaceholderDesc.ts
+++ src/Pages/PreviewCourse/PlaceholderDesc.ts
@@ -6,7 +6,8 @@ const DescContent = [
         dataRelease: "26/11/2023",
         bannerTitle: "Curso completo de Malwares, Ransomwares e outros vírus",
         bannerText: "Aulas Completas de Malwares. Saiba como se proteger e mitigar tais ataques em ambientes empresariais.",
-        card_content: [
+        rating: 4.8,
+        learnings: [
             {
                 topico: 1,
                 content: "Aprenda a importância de se ter um ambiente seguro",
@@ -24,10 +25,27 @@ const DescContent = [
             {
                 topico: 4,
                 content: "Aprenda a identificar e solucionar vulnerabilidades de Malware em um ambiente corporativo",
-            }
+            },
+            {
+                topico: 5,
+                content: "Aprenda a importância de se ter um ambiente seguro",
+            },
+            {
+                topico: 6,
+                content: "Desenvolva habilidades para responder eficientemente a incidentes de segurança, minimizando danos e restaurando a integridade do sistema.",
+              },
+              {
+                topico: 7,
+                content: "Saiba como conduzir auditorias de segurança para avaliar a eficácia das medidas de segurança implementadas em uma organização.",
+              },
+              {
+                topico: 8,
+                content: "Adote boas práticas para garantir a segurança contínua em ambientes corporativos, incluindo políticas de segurança, conscientização dos usuários e manutenção regular.",
+              }
         ]
-
     }
+
+
 ]
 
 export default DescContent;
--- src/Pages/PreviewCourse/PreviewCourse.tsx
+++ src/Pages/PreviewCourse/PreviewCourse.tsx
@@ -1,38 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../../Components/Header/Header'
 import Footer from '../../Components/Footer/Footer'
+import DescContent from './PlaceholderDesc'
+import { ExclamationCircleFill, Star, StarFill, Globe, Check } from 'react-bootstrap-icons'
 import './style.scss'
 
-const DescContent = [
-    {
-        id: 1,
-        name: "Malwares, Ransomwares e outros vírus",
-        teacher: ["Julia Beatriz", "Camilly Souza"],
-        dataRelease: "26/11/2023",
-        bannerTitle: "Curso completo de Malwares, Ransomwares e outros vírus",
-        bannerText: "Aulas Completas de Malwares. Saiba como se proteger e mitigar tais ataques em ambientes empresariais.",
-        card_content: [
-            {
-                topico: 1,
-                content: "Aprenda a importância de se ter um ambiente seguro",
-            },
-            {
-                topico: 2,
-                content: "Aprenda a identificar os tipos de Malware",
-            },
-            {
-                topico: 3,
-                content: "Aprenda os danos que um Malware pode causar",
-            },
-            {
-                topico: 4,
-                content: "Aprenda a identificar e solucionar vulnerabilidades de Malware em um ambiente corporativo",
-            }
-        ]
-    }
-];
-
 const PreviewCourse = () => {
+    const stars = []
+    
+    for (let i = 0; i < Math.round(DescContent[0].rating); i++) {
+        stars.push(<StarFill fill="#3b82f6" />)
+    } for (let i = stars.length; i < 5; i++) {
+        stars.push(<Star />)
+    }
+
+    const [isExpanded, setIsExpanded] = useState(false)
+
+    const handleExpand = () => {
+        setIsExpanded(!isExpanded)
+    }
+
     return (
         <>
             <Header />
@@ -46,27 +33,39 @@ const PreviewCourse = () => {
 
                <div className="banner__description">
                     <p>Criado por <span>{DescContent[0].teacher[0]}</span> e <span>{DescContent[0].teacher[1]}</span></p>
+
+                    <div className="banner__ratings">
+                            <div className="banner__ratings__stars">
+                                    {stars}
+                            </div>
+                            {DescContent[0].rating}
+                    </div>
                     <br />
                 <section className='banner__description__lang'>
-                    <p>Última atualização em {DescContent[0].dataRelease}</p>
-                    <p>Idioma: Português [Brasil]</p>
+                    <p><ExclamationCircleFill /> Última atualização em {DescContent[0].dataRelease}</p>
+                    <p><Globe /> Português [Brasil]</p>
                 </section>
                </div>
             </div>
 
-            {/* <div className="card">
+            <div className={`card ${isExpanded ? 'expanded' : ''}`}>
                 <div className="card__title">
                     O que você aprenderá
                 </div>
                 <div className="card__description">
-                    {DescContent[0].card_content.map((item) => (
+                    {DescContent[0].learnings.map((item) => (
                         <div key={item.topico}>
-                            <p>{`Tópico ${item.topico}: ${item.content}`}</p>
+                            <p>{<Check />}{item.content}</p>
                         </div>
                     ))}
                 </div>
-            </div> */}
-        <Footer />
+            <div className="card__expand__blur">
+            </div>
+                <div className="card__expand"  onClick={handleExpand}>
+                    <a>{isExpanded ? 'Mostrar menos' : 'Mostrar mais'}</a>
+                </div>
+            </div>
+            <Footer />
         </>
     );
 };
